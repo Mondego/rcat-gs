0ef39826b608f7f003939cc9aaf3e750a8b131ce
avaladar <avaladar@be55aff0-df03-3d4e-954c-2c4342f5ed18>
2011-04-24 01:19:13 +0000

    Forgot to add/delete files on last commit
    git-svn-id: https://opensim.ics.uci.edu/svn/RCAT/trunk@20 be55aff0-df03-3d4e-954c-2c4342f5ed18

---
+++ RCAT-Server/RCAT/RCATContext.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace RCAT
+{
+    public class RCATContext : IDisposable
+    {
+        // TODO: Default buffer size. Should set this somewhere global?
+        public static int DefaultBufferSize = 4096;
+        public StringBuilder sb = new StringBuilder();
+        public byte[] buffer = new byte[DefaultBufferSize];
+        public TcpClient proxyConnection;
+        public Message message;
+        public SemaphoreSlim ReceiveReady = new SemaphoreSlim(1);
+
+        public void Send(string Data)
+        {
+            Send(UTF8Encoding.UTF8.GetBytes(Data));
+        }
+
+        public void Broadcast(dynamic data, string[] clients)
+        {
+            ClientBroadcast cb = new ClientBroadcast();
+            cb.data = message.Data;
+            cb.clients = clients;
+            Send(Newtonsoft.Json.JsonConvert.SerializeObject(cb));
+        }
+
+        /// <summary>
+        /// Sends the specified data.
+        /// </summary>
+        /// <param name="Data">The data.</param>
+        /// <param name="AContext">The user context.</param>
+        /// <param name="Close">if set to <c>true</c> [close].</param>
+        public void Send(byte[] Data)
+        {
+            AsyncCallback ACallback = EndSend;
+            try
+            {
+                proxyConnection.Client.BeginSend(Data, 0, Data.Length, SocketFlags.None, ACallback, this);
+            }
+            catch
+            {
+                Console.WriteLine("[ServerContext]: Exception sending");
+                //AContext.SendReady.Release();
+            }
+        }
+
+        /// <summary>
+        /// Ends the send.
+        /// </summary>
+        /// <param name="AResult">The Async result.</param>
+        public void EndSend(IAsyncResult AResult)
+        {
+            RCATContext RContext = (RCATContext)AResult.AsyncState;
+            try
+            {
+                RContext.proxyConnection.Client.EndSend(AResult);
+                //AContext.SendReady.Release();
+            }
+            catch
+            {
+                Console.WriteLine("[ServerContext]: Exception end send");
+                //AContext.SendReady.Release(); 
+            }
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
